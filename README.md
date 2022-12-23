39c40966631be98c8b746c08f18cb98ef1c7b046
Ben Cho <ben.cho@gmail.com>
2022-12-22 23:28:47 -0700

    added readme and fixed mod name

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cython Test Package
+
+Test python package for cython extensions as part of base
+package with with working pyx file and github actions for
+building and deploying package to pypi.
--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ import numpy
 
 setup(
     name="cython_numpy_test",
-    version="0.0.5",
+    version="0.0.6",
     install_requires=[
         "numpy",
         "cython",
     ],
+    ext_package='cython_numpy_test',
     ext_modules = Cython.Build.cythonize("cython_test.pyx"),
     include_dirs=[numpy.get_include()]
     )
