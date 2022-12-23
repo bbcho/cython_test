746e5db6e212c3bb10b1c22ca9dfa681eb85b122
Ben Cho <ben.cho@gmail.com>
2022-12-22 21:28:26 -0700

    added requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ import Cython.Build
 import numpy
 
 setup(
+    install_requires=[
+        "numpy",
+        "cython",
+    ],
     ext_modules = Cython.Build.cythonize("test_cython.pyx"),
     include_dirs=[numpy.get_include()]
     )
